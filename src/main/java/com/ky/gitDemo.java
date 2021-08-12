8d98411d43ec59c514bfbb7052772c9cdb489b1f
hb <993486215@qq.com>
2021-08-12 15:19:47 +0800

    Second commit

--- src/main/java/com/ky/gitDemo.java
+++ src/main/java/com/ky/gitDemo.java
@@ -3,5 +3,6 @@ package com.ky;
 public class gitDemo {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello hot-fix");
     }
 }
